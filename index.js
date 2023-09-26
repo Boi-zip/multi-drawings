14fe8ac018926b29070169bb9c5caf7ba6d4b29d
Boi.zip <57861832+Boi-zip@users.noreply.github.com>
2023-09-26 07:54:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 
-mongoose.connect('process.env.MONGOCN', {
+mongoose.connect(process.env.MONGOCN, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
